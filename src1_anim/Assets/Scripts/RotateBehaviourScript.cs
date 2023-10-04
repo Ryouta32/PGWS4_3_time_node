2cf38f02d267440146fcac3df374951df41991a8
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2023-10-04 18:05:51 +0900

    授業でやったことの追加

--- src1_anim/Assets/Scripts/RotateBehaviourScript.cs
+++ src1_anim/Assets/Scripts/RotateBehaviourScript.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class RotateBehaviourScript : MonoBehaviour
 {
+
     void Update()
     {
+        float rotate = 360 * Time.deltaTime/10f;
+
+        this.transform.Rotate(0, rotate, 0);
     }
 }
